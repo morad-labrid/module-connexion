5f89176261c1237981dac94f0f6ed0dc6970b9c6
Morad <morad.labrid@laplateforme.io>
2020-11-16 12:36:58 +0100

    Page d'accueil

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <title>Good Vibes</title>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+        }
+        body{
+            background-color: #0E074B;
+            color: white;
+            font-family: 'Archivo', cursive, sans-serif;
+            padding: 0 100px;
+        }
+        header div h1{
+            font-family: 'Pacifico', cursive, sans-serif;
+            margin: 0;
+        }
+        a{
+            text-decoration: none;
+            color: inherit;
+        }
+        header{
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding-top: 10px;
+        }
+        main section div p{
+            width: 400px;
+            margin-top: 50px;
+        }
+        img{
+            width: 450px;
+        }
+        section{
+            margin-top: 150px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+
+    
+    
+    </style>
+</head>
+<body>
+    <header>
+        <div>
+            <h1>VIBES</h1>
+        </div>
+        <nav>
+            <a href="">Inscription</a>
+            <a href="">Connexion</a>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <div>
+                <h1>Trouvez votre vibe</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In libero omnis repellat doloribus expedita. Dignissimos, quasi reiciendis earum nobis libero doloremque nihil neque harum quidem nostrum, voluptas illo enim fugit.</p>
+            </div>
+            <div>
+                <img src="assets/banner1-3.png" alt="Photo">
+            </div>
+        </section>
+    
+    </main>
+    <footer>
+        <div>
+            <p>Copyright 2020</p>
+        </div>
+    </footer>
+</body>
+</html>

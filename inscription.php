411c5611e6fcf39b46596114684844e7871fe75b
samy <samy>
2020-11-16 17:49:07 +0100

    1er stage que j'ai commencé mais probleme de connexion mourad a pris la releve

---
+++ inscription.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+<form action="connexion.php" method="POST" >
+<label for="" ></label>
+<input type="text" name="login" value="s'inscrire">
+<label for=""></label>
+<input type="text" name="nom" value="">
+<label for=""></label>
+<input type="text" name="prenom" value="" >
+<label for=""></label>
+<input type="password" name="password" value="" >
+<label for=""></label>
+<input type="password" name="confirm_password" value="" >
+
+
+
+</form>
